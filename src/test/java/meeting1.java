370fd0d955ccb3e234a7823c4f049ff96d480d69
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-04-07 22:56:09 -0500

    add webDriver Manager add WebDriver dependency into pom.xml file

--- src/test/java/meeting1.java
+++ src/test/java/meeting1.java
@@ -1,14 +1,16 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import java.util.List;
 
 public class meeting1 {
+
     @Test
     public void firstTest() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("http://www.google.com");
         Thread.sleep(3000);
@@ -18,6 +20,8 @@ public class meeting1 {
         WebElement submitBtn = driver.findElement(By.cssSelector("[type='submit']"));
         submitBtn.click();
         Thread.sleep(3000);
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
