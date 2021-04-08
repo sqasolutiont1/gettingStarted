2592a5fe9297e9aa99bd02cec7d12773fc7386bc
jsalazar4 <jsalazar@acid.cl>
2021-04-07 23:54:23 -0400

    Add driver.quit

--- src/test/java/meeting1.java
+++ src/test/java/meeting1.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,15 +10,19 @@ import java.util.List;
 public class meeting1 {
     @Test
     public void firstTest() throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("http://www.google.com");
         Thread.sleep(3000);
-        WebElement searchField = driver.findElement(By.cssSelector("[title='Search']"));
+        WebElement searchField = driver.findElement(By.cssSelector("[title='Buscar']"));
         searchField.sendKeys("Testing is awesome!");
         Thread.sleep(3000);
         WebElement submitBtn = driver.findElement(By.cssSelector("[type='submit']"));
         submitBtn.click();
         Thread.sleep(3000);
-        driver.quit();
+        if(driver != null){
+            driver.quit();
+        }
     }
 }
