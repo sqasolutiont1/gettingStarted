642b51a98a26f55650e8e4024a49b71c0e21f2a7
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-04-10 16:05:33 -0500

    add Home Page PO

--- src/test/java/vlad/Tests.java
+++ src/test/java/vlad/Tests.java
@@ -8,26 +8,23 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import vlad.pagesObjects.HomePO;
 
 public class Tests {
-    WebDriver webDriver = null;
+    HomePO homePO = new HomePO();
     @BeforeMethod
     public void openTheBrowser(){
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
-        webDriver.navigate().to("http://authenticgoods.co/wrapbootstrap/themes/neuboard-v1.4.3/Angular_full_version/index.html#/forms/multi-upload");
+        homePO.OpenHomePage();
     }
 
     @Test
-    public void getTitle() throws InterruptedException {
-        Thread.sleep(5000);
-        Assert.assertEquals(webDriver.getTitle(),"CoreUI - Free Angular Admin Template",
+    public void checkTitle() throws InterruptedException {
+        Assert.assertEquals(homePO.getTitle(),"NeuBoard | Multiple File Upload",
                     "The Title is not what we are expecting");
     }
 
     @AfterMethod
     public void closeBrowser(){
-        webDriver.quit();
-        webDriver = null;
+        homePO.closeEverything();
     }
 }
