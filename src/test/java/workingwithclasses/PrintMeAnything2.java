3723ddfbaa0f6d60f2e13a5500039fe03fce55f0
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-04-10 12:45:39 -0500

    add new branch add PrintMeAnything2.java class

---
+++ src/test/java/workingwithclasses/PrintMeAnything2.java
@@ -0,0 +1,20 @@
+package workingwithclasses;
+
+public class PrintMeAnything2 {
+    public PrintMeAnything2(){
+        System.out.println("this is constructor");
+    }
+
+    public static int numbers = 2134234;
+    public void printMeString(String value){
+        System.out.println(value);
+    }
+
+    private void youdontseeme(){
+        System.out.println("nope");
+    }
+
+    public static void printmeregardless(){
+        System.out.println("yup");
+    }
+}
