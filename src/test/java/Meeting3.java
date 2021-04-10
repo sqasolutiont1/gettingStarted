9d2f650d3a44128c343922d3b8b56e423c093f13
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-04-10 12:01:53 -0500

    init

---
+++ src/test/java/Meeting3.java
@@ -0,0 +1,15 @@
+public class Meeting3 {
+    public static void main(String... args){
+        printme();
+    }
+
+    public static void printme(){
+        System.out.println("Something");
+        System.out.println(getName());
+
+    }
+    private static String getName(){
+        return "Vlad";
+    }
+
+}
