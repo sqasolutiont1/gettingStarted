938c7dc87cb38b42b31fa11f6e1228c2a4d996ed
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-04-10 16:08:34 -0500

    add working with Strings init

---
+++ src/test/java/vlad/JavaExamples/workingwithclasses/PrintMeAnything.java
@@ -0,0 +1,21 @@
+package vlad.JavaExamples.workingwithclasses;
+
+public class PrintMeAnything {
+    public PrintMeAnything(){
+        System.out.println("this is constructor");
+    }
+
+    public static int numbers = 2134234;
+    public void printMeString(String value){
+        System.out.println(value);
+    }
+
+    private void youdontseeme(){
+        System.out.println("nope");
+    }
+
+    public static void printmeregardless(){
+        System.out.println("yup");
+    }
+
+}
