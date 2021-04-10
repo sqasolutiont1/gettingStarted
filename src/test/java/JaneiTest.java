12b73e9d350f27f3b60a394bcdc22edd5c05079f
Janeislzr <janeicontrerass@gmail.com>
2021-04-10 13:43:08 -0400

    add to my new branch

---
+++ src/test/java/JaneiTest.java
@@ -0,0 +1,12 @@
+public class JaneiTest {
+    public static void main(String[] args){
+
+
+    }
+
+    public static void printme(){
+        System.out.println("Something");
+    }
+
+}
+
--- src/test/java/workingwithclasses/PrintMeAnything.java
+++ src/test/java/workingwithclasses/PrintMeAnything.java
@@ -1,20 +1,7 @@
 package workingwithclasses;
 
 public class PrintMeAnything {
-    public PrintMeAnything(){
-        System.out.println("this is constructor");
-    }
-
-    public static int numbers = 2134234;
-    public void printMeString(String value){
+    public String printMeString(String value){
         System.out.println(value);
-    }
-
-    private void youdontseeme(){
-        System.out.println("nope");
-    }
-
-    public static void printmeregardless(){
-        System.out.println("yup");
     }
 }
