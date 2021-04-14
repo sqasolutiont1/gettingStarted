85238aac0d86de83873a985b125c2db36e8de0a6
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-04-14 18:58:06 -0500

    add Tasks 1

--- src/main/java/Tasks1.java
+++ src/main/java/Tasks1.java
@@ -15,4 +15,37 @@ public class Tasks1 {
         }
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
+
+
+    public Integer find(Integer[] arr) {
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                secondMax = max;
+                max = arr[i];
+            }
+
+            if (arr[i] < max && arr[i] > secondMax) {
+                secondMax = arr[i];
+            }
+        }
+        return secondMax;
+    }
+
+    public Integer evenFibonacciSum(Integer n) {
+        int previousFibonacci = 1;
+        int currentFibonacci = 2;
+        int evenFibonacciSum = 0;
+        do {
+            if (currentFibonacci % 2 == 0) {
+                evenFibonacciSum += currentFibonacci;
+            }
+            int newFibonacci = currentFibonacci + previousFibonacci;
+            previousFibonacci = currentFibonacci;
+            currentFibonacci = newFibonacci;
+        } while (currentFibonacci < n);
+        return evenFibonacciSum;
+    }
 }
