7730d0dc5e463c55f346a539cf811ae4690cad6f
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-04-14 20:10:01 -0500

    add Inheritance

--- src/main/java/Examples/ooConcepts/Inheritance.java
+++ src/main/java/Examples/ooConcepts/Inheritance.java
@@ -69,7 +69,6 @@ package Examples.ooConcepts;
     public class Inheritance {
         public static void main(String args[])
         {
-
             MountainBike mb = new MountainBike(3, 100, 25);
             System.out.println(mb.toString());
         }
