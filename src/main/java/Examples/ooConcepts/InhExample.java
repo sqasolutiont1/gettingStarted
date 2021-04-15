da779ee43d1bd3ce0524872ead5a4cc785bb8de9
vladimir_kamenev <vladimir_kamenev@sqasolution.com>
2021-04-14 20:41:29 -0500

    add Inheritance, more examples

---
+++ src/main/java/Examples/ooConcepts/InhExample.java
@@ -0,0 +1,9 @@
+package Examples.ooConcepts;
+
+import Examples.ooConcepts.Parents.PrintHelloWorld2;
+
+public class InhExample {
+    private static void methodToDemoThings(){
+        PrintHelloWorld2.getMeURLFromThePropertyFile();
+    }
+}
---
+++ src/main/java/Examples/ooConcepts/Parents/PrintHelloWorld2.java
@@ -0,0 +1,10 @@
+package Examples.ooConcepts.Parents;
+
+public class PrintHelloWorld2 {
+    public static void getMeURLFromThePropertyFile(){
+        System.out.println("Hello, world!");
+    }
+    public void printItNow(){
+        System.out.println("Hello, world!");
+    }
+}
