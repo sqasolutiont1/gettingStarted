565d3d47669f5be8f2af2ee6c87f2561aee66d82
Janeislzr <janeicontrerass@gmail.com>
2021-05-01 13:21:04 -0400

    wizardHP.feature

--- src/test/java/etrade/PageObjects/wizard/RecurringDeposits.java
+++ src/test/java/etrade/PageObjects/wizard/RecurringDeposits.java
@@ -1,10 +1,16 @@
 package etrade.PageObjects.wizard;
 
+import org.openqa.selenium.By;
+
 public class RecurringDeposits extends WizardBasePO{
     String url = "https://us.etrade.com/etx/wm/core-portfolios/rtq/3?neo.skin=mininav";
 
     public void getMeToRecurringDeposits(){
         navigateTo(url);
     }
+    public void insertGoal(String goal){
+        getClickableElement(By.xpath("//input[@id='4jxpXtBVs']")).sendKeys(goal);
+    }
+    public void setfrecuenct(){}
 
 }
