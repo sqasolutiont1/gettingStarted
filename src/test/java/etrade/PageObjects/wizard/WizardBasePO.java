7feb56ea6021bd79faa4a366655b3febdccc044e
karthikkrishnan1986 <karthikkrishnan1986@gmail.com>
2021-05-01 10:21:40 -0700

    KK

--- src/test/java/etrade/PageObjects/wizard/WizardBasePO.java
+++ src/test/java/etrade/PageObjects/wizard/WizardBasePO.java
@@ -4,6 +4,10 @@ import PageObjects.BaseClasses.BasePageObject;
 import org.openqa.selenium.By;
 
 public class WizardBasePO extends BasePageObject {
+
+    public void enterAmt(){
+        getClickableElement(By.xpath(""))
+    }
     public void pressContinueBtn(){
         getClickableElement(By.cssSelector("[type=\"button\"]")).click();
     }
